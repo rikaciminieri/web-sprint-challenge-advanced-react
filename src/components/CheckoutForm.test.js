8c94a88e828a1d1060a07386c7a7f8600a83b188
Rika Ciminieri <rikaciminieri@gmail.com>
2021-07-18 20:53:04 -0700

    Tests Passing, MVP Complete

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -1,9 +1,19 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+  render(<CheckoutForm />);
+  const header = document.querySelector("h2");
+  expect(header).toBeInTheDocument;
+});
 
-test("form shows success message on submit with form details", () => {});
+test("form shows success message on submit with form details", async () => {
+  render(<CheckoutForm />);
+  const submitButton = screen.getByRole("button");
+  await fireEvent.click(submitButton);
+  const successMessage = screen.getByTestId("successMessage");
+  expect(successMessage).toBeVisible;
+});
