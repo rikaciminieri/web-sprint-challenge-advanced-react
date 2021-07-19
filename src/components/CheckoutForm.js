16e73cbbe08832e3478508ae312a1697fc9678de
Rika Ciminieri <rikaciminieri@gmail.com>
2021-07-18 19:32:03 -0700

    PlantList working with API call

--- src/components/CheckoutForm.js
+++ src/components/CheckoutForm.js
@@ -18,10 +18,6 @@ const CheckoutForm = (props) => {
   const [showSuccessMessage, setShowSuccessMessage] = useState(false);
   const [values, handleChanges] = useForm(initialValue);
 
-  // const handleChanges = (e) => {
-  //   setValues({ ...values, [e.target.name]: e.target.value });
-  // };
-
   const handleSubmit = (e) => {
     e.preventDefault();
     setShowSuccessMessage(true);
