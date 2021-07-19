c40360c076a8ca90d7ca1b8e379414e5e18e6bac
Rika Ciminieri <rikaciminieri@gmail.com>
2021-07-18 18:03:52 -0700

    CodeGrade test

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -3,7 +3,10 @@ import axios from "axios";
 
 export default class PlantList extends Component {
   // add state with a property called "plants" - initialize as an empty array
-
+constructor(props) {
+  super(props);
+  this.state = {plants: []};
+}
   // when the component mounts:
   //   - fetch data from the server endpoint - http://localhost:3333/plants
   //   - set the returned plants array to this.state.plants
